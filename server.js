b45f06261e5a68b2632b2ac080288ae153d211ff
Russ yev <ruslanyev@gmail.com>
2018-10-17 19:18:05 -0700

    fixed my quotes issue

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-const blogPostsRouter = require('/blogPostsRouter');
+const blogPostsRouter = require("/blogPostsRouter");
 
-app.use(morgan('common'));
+app.use(morgan("common"));
 
-app.use('/blog-posts', blogPostsRouter);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname);
+});
+
+app.use("/blog-posts", blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
